f0fc6f2f672dd75b1f762006591246a02919d73d
Black-Udaff <xxxalex_93@icloud.com>
2024-04-28 18:24:57 +0300

    123

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -29,12 +29,14 @@ INSTALLED_APPS = [
     'django_filters',
     'djoser',
     'users',
+    "corsheaders",
     'ricept',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -168,3 +170,6 @@ DJOSER = {
     'HIDE_USERS': False,
 
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
